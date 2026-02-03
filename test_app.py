edff39e9ec1f7ca3020a63f541370128a867c513
Cratosaris <aristideogbwoy@gmail.com>
2026-02-03 14:31:40 +0200

    Correction du fichier de test

--- test_app.py
+++ test_app.py
@@ -2,3 +2,8 @@ from app import addition
 
 def test_addition():
     assert addition(2, 3) == 5
+
+ from app   import multiplication
+
+ def test_multiplication
+     assert multiplication(5,6) == 30   
